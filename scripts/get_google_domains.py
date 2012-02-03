28e1bf43411e13f7707960b842f8544d79497643
cybunk <admin@cybunk.com>
2012-02-03 19:59:27 +0100

    FIX : tld bug lost some countries

--- scripts/get_google_domains.py
+++ scripts/get_google_domains.py
@@ -3,6 +3,7 @@
 #
 # "get_google_domains.py"
 # By Antoine Mazières -- http://ant1.cc/
+# fix to have .com.tld by sam
 # "AS IS" under CC_BY_SA
 #
 
@@ -13,19 +14,20 @@ reload(sys)
 sys.setdefaultencoding("utf-8")
 ##
 
-d = urllib.urlopen('http://www.google.com/supported_domains').read().replace('\n', '').split('.google')[1:]
-
+d = urllib.urlopen('http://www.google.com/supported_domains').read().replace('\n', '').split('.')[1:]
 country_tld = {}
 
 with open ('../documentation/countrylist.csv', 'rb') as f:		#List found here: http://www.andrewpatton.com/countrylist.csv
-	reader = csv.reader(f)
+	reader = csv.reader(f)	
 	for row in reader:
 		if 'and' not in row[-1]:
-			if row[-1] in d:
-				country_tld[row[1]] = row[-1]
+			row[-1]= row[-1].split('.')
+			if row[-1][-1] in d:
+				country_tld[row[1]] = row[-1][-1]
 		else:
 			for each in row[-1].split(' and '):
-				if each in d:
-					country_tld[row[1]] = each
-
+				each = each.split('.')
+				if each[-1] in d:
+					country_tld[row[1]] = each[-1]
+print "Result : "
 print country_tld
